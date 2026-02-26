deab2e7fa74b4e336e0e5fe20cda7249b1a0ba0d
alishababar <alishababar945@gmail.com>
2026-02-27 02:45:31 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-import Login from "@/components/ui/auth/login-form";
-
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4 text-center">Welcome to the Todo App!</h1>
-      <Login />
+    <div>
+      <h1>Todo-App</h1>
     </div>
   );
 }
