54773886f6702a48a770944d1c941e1863f63518
alishababar <alishababar945@gmail.com>
2026-03-23 00:16:14 +0500

    Setup Prisma with PostgreSQL

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,57 +1,50 @@
-// export const signUp = async (name: string, email: string, password: string) => {
-//   const res = await fetch("/api/auth/sign-up/email", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ name, email, password }),
-//   });
+export const signUp = async (name: string, email: string, password: string) => {
+  const res = await fetch("/api/auth/sign-up/email", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, email, password }),
+  });
 
-//   const data = await res.json();
+  const data = await res.json();
 
-//   if (!res.ok) {
-//     console.error(data);
-//     throw new Error("Sign up failed");
-//   }
+  if (!res.ok) {
+    console.error(data);
+    throw new Error("Sign up failed");
+  }
 
-//   return data;
-// };
+  return data;
+};
 
-// export const signIn = async (email: string, password: string) => {
-//   const res = await fetch("/api/auth/sign-in/email", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ email, password }),
-//   });
+export const signIn = async (email: string, password: string) => {
+  const res = await fetch("/api/auth/sign-in/email", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
 
-//   const data = await res.json();
+  const data = await res.json();
 
-//   if (!res.ok) {
-//     console.error(data);
-//     throw new Error("Sign in failed");
-//   }
+  if (!res.ok) {
+    console.error(data);
+    throw new Error("Sign in failed");
+  }
 
-//   return data;
-// };
+  return data;
+};
 
-// export const signOut = async () => {
-//   const res = await fetch("/api/auth/sign-out", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({})
-//   });
+export const signOut = async () => {
+  const res = await fetch("/api/auth/sign-out", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({})
+  });
 
-//   if (!res.ok) {
-//     throw new Error("Logout failed");
-//   }
+  if (!res.ok) {
+    throw new Error("Logout failed");
+  }
 
-//   return res;
-// };
+  return res;
+};
 
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
-});
-
-export const { signIn, signUp, signOut, useSession } = authClient;
